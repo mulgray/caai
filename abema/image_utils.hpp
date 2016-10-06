ff1c964902607deca85c6754dbe663f70c92926a
haripo <ehutoad@gmail.com>
2016-10-06 16:23:18 +0900

    Fix blending

--- abema/image_utils.hpp
+++ abema/image_utils.hpp
@@ -6,23 +6,29 @@ class ImageUtils {
 public:
   static void blend(cv::Mat src, cv::Mat dest) {
     std::vector<cv::Mat> src_channels;
-    cv::Mat alpha_map, src_alpha, src_rgb;
+    cv::Mat alpha_map;
+    cv::Mat src_rgb;
 
     cv::split(src, src_channels);
-    cv::normalize(src_channels[3], alpha_map, 0.0f, 1.0f, cv::NORM_MINMAX);
+
+    src_channels[3].convertTo(alpha_map, CV_32FC1);
+    alpha_map /= 255.0;
 
     cv::merge(std::vector<cv::Mat> {
-      alpha_map,
-      alpha_map,
-      alpha_map
-    }, src_alpha);
-
-    cv::merge(std::vector<cv::Mat> {
-      src_channels[0],
+      src_channels[2],
       src_channels[1],
-      src_channels[2]
+      src_channels[0]
     }, src_rgb);
 
-    dest = dest.mul(cv::Scalar::all(1) - src_alpha) + src_rgb.mul(src_alpha);
+    for (int i = 0; i < dest.rows; i++) {
+      for (int j = 0; j < dest.cols; j++) {
+        float a = alpha_map.at<float>(i, j);
+        for (int k = 0; k < 3; k++) {
+          dest.at<cv::Vec3b>(i, j)[k] =
+            dest.at<cv::Vec3b>(i, j)[k] * (1 - a) +
+            src_rgb.at<cv::Vec3b>(i, j)[k] * a;
+        }
+      }
+    }
   }
 };
